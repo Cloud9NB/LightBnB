ee6e6c1a840f297b692e9d5c0ab0ed81905a409e
Kevin Lee <blackseeds10@hotmail.com>
2022-04-08 00:18:12 +0000

    change addProperty function to sql querry

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -136,7 +136,7 @@ const getAllProperties = (options, limit = 10) => {
       queryString += ` WHERE `;
     }
     queryParams.push(options.owner_id);
-    queryString += ` owner_id = $${queryParams.length} `
+    queryString += ` owner_id = $${queryParams.length} `;
   }
 
   if (options.minimum_price_per_night) {
@@ -146,7 +146,7 @@ const getAllProperties = (options, limit = 10) => {
       queryString += `WHERE `;
     }
     queryParams.push(options.minimum_price_per_night);
-    queryString += ` cost_per_night > $${queryParams.length} `
+    queryString += ` cost_per_night > $${queryParams.length} `;
   }
 
   if (options.maximum_price_per_night) {
@@ -156,7 +156,7 @@ const getAllProperties = (options, limit = 10) => {
       queryString += ` WHERE `;
     }
     queryParams.push(options.maximum_price_per_night);
-    queryString += ` cost_per_night < $${queryParams.length} `
+    queryString += ` cost_per_night < $${queryParams.length} `;
   }
 
   queryString += `
@@ -175,10 +175,10 @@ const getAllProperties = (options, limit = 10) => {
   `;
 
   return pool.query(queryString, queryParams)
-  .then(res => {
-    return res.rows;
-  })
-  .catch((err) => {
+    .then(res => {
+      return res.rows;
+    })
+    .catch((err) => {
       console.log(err.message);
     });
 };
@@ -191,9 +191,28 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool.query(
+    `INSERT INTO properties (
+      owner_id,
+      title,
+      description,
+      thumbnail_photo_url,
+      cover_photo_url,
+      cost_per_night,
+      parking_spaces,
+      number_of_bathrooms,
+      number_of_bedrooms,
+      country,
+      street,
+      city,
+      province,
+      post_code
+    )
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;
+    `, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+    .then(res => {
+      return res.rows;
+    });
 };
 exports.addProperty = addProperty;
